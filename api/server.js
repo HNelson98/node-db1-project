a88cbbd682bfd360fbc9f42a165a967dfa740399
Henry Nelson <Henry.Nelson89@gmail.com>
2020-05-11 13:51:51 -0700

    crud operations are all functional for the accounts

--- api/server.js
+++ api/server.js
@@ -6,4 +6,82 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.json('HELOO')
+})
+
+server.get('/api/accounts', (req, res) => {
+    db.select('*').from('accounts').then(account => {
+        res.status(200).json({ data: account })
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: error.message });
+
+    });
+})
+
+server.get('/api/accounts/:id', (req, res) => {
+    db('accounts').where({ id: req.params.id })
+        .then(account => {
+            res.status(200).json({ data: account })
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({ message: error.message });
+
+        });
+})
+
+server.post('/api/accounts', (req, res) => {
+    const account = req.body
+    if (isValidAccount(account)) {
+        db('accounts')
+            .insert(account, "id")
+            .then(ids => {
+                res.status(201).json({ data: ids })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json({ message: error.message });
+            })
+    } else {
+        res.status(400).json({ message: 'Please provide a budget and a name' })
+    }
+})
+
+server.put('/api/accounts/:id', (req, res) => {
+    const account = req.body
+
+    if (isValidAccount(account)) {
+        db('accounts').where({ id: req.params.id }).update(account)
+            .then(newAccount => {
+                res.status(201).json(newAccount)
+            }).catch(err => {
+                console.log(err);
+                res.status(500).json({ message: error.message });
+
+            });
+    } else {
+        res.status(400).json({ message: 'Please provide a budget and a name' })
+    }
+})
+
+server.delete('/api/accounts/:id', (req, res) => {
+    db('accounts').where({ id: req.params.id })
+    .delete()
+    .then(succsess => {
+        res.status(201).json("Account Deleted")
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ message: error.message });
+
+    });
+})
+
+
+
+//custom middleware
+function isValidAccount(account) {
+    return Boolean(account.name && account.budget);
+}
+
 module.exports = server;
